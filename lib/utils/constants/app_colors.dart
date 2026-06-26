0d4da6c25c3cf40c592088eccfbcc223c912b856
Huzayfa Sani <huzaifa0133@gamil.com>
2026-06-26 21:17:32 +0600

    Folder Structure Setup 1

---
+++ lib/utils/constants/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primaryColor = Color(0xFF46000B);
+  static const Color secondary = Color(0xff454545);
+  static const Color background = Color(0xFFFDF9F9);
+
+  static const Color transparent = Colors.transparent;
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color red = Colors.red;
+  static const Color filledColor = Color(0xFFF9FAFB);
+  static const Color textFiledColor = Color(0xFF979797);
+  static const blueLight = Color(0xffe8e8f5);
+}
---
+++ lib/utils/constants/app_images.dart
@@ -0,0 +1,23 @@
+class AppImages {
+  AppImages._();
+
+  static const String baseurl = 'assets/images';
+  static const String iconUrl = 'assets/icons';
+
+  static const String douxis = '$baseurl/douxis.png';
+  static const String noData = '$baseurl/noData.png';
+  static const String noImage = '$baseurl/noImage.png';
+  static const String profile = '$baseurl/profile.png';
+  static const String logo = '$baseurl/logo.png';
+  static const String onbording = '$baseurl/onbording.png';
+  static const String splash_Screen = '$baseurl/splash_Screen.png';
+
+  static const String appLogo = '$iconUrl/temp_logo.svg';
+  static const String email_icon = '$iconUrl/email_icon.svg';
+  static const String pass_lock_icon = '$iconUrl/pass_lock_icon.svg';
+  static const String notification_icon = '$iconUrl/notification_icon.svg';
+  static const String home_icon = '$iconUrl/home_icon.svg';
+  static const String order_icon = '$iconUrl/order_icon.svg';
+  static const String profile_icon = '$iconUrl/profile_icon.svg';
+  static const String wallet_icon = '$iconUrl/wallet_icon.svg';
+}
