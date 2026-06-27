a597f6b4ae480be06c095a1293557e3d14f8a4c0
Huzayfa Sani <huzaifa0133@gamil.com>
2026-06-27 10:13:35 +0600

    update

--- lib/utils/constants/app_colors.dart
+++ lib/utils/constants/app_colors.dart
@@ -5,7 +5,7 @@ class AppColors {
   static const Color primaryColor = Color(0xFF46000B);
   static const Color secondary = Color(0xff454545);
   static const Color background = Color(0xFFFDF9F9);
-  static const Color white = Color(0xFFFDF9F9);
+  static const Color whiteNormal = Color(0xFFFDF9F9);
   static const Color back = Color(0xFFFDdf9F9);
 
   static const Color transparent = Colors.transparent;
